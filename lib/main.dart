11a4007b131659c513ab993418bcf8f44ecc68bf
probirroy <probirroy2@gmail.com>
2023-11-18 18:28:41 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -70,11 +70,11 @@ class _ExampleState extends State<Example> {
               gap: 8,
               activeColor: Colors.black,
               iconSize: 24,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              duration: const Duration(milliseconds: 400),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
               tabBackgroundColor: Colors.grey[100]!,
               color: Colors.black,
-              tabs: const [
+              tabs: [
                 GButton(
                   icon: LineIcons.home,
                   text: 'Home',
